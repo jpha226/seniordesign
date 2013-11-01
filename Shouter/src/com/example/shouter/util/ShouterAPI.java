d2917f3c448144dda0aab228adc16e7fd853dfa5
Craig <cschmidt489>
2013-11-01 16:45:22 -0400

    After fixing conflicts committed api and delegate

--- Shouter/src/com/example/shouter/util/ShouterAPI.java
+++ Shouter/src/com/example/shouter/util/ShouterAPI.java
@@ -1,3 +1,4 @@
+
 package com.example.shouter.util;
 
 import java.io.IOException;
@@ -7,16 +8,23 @@ import java.util.concurrent.Executors;
 import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.JsonMappingException;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import com.androidtools.Networking;
 import com.example.shouter.Shout;
+
+
 
 public class ShouterAPI {
 
-	private static final String Shouter_URL = "INSERT URL";
+	private static final String Shouter_URL = "http://shouterapi-env.elasticbeanstalk.com";
 	
 	private ExecutorService executor;
 	private ShouterAPIDelegate delegate;
+	private RestTemplate REST = Networking.defaultRest();
 
 	private String shoutString;
 
@@ -30,7 +38,11 @@ public class ShouterAPI {
 		delegate = d;
 	}
 	
-	public void postShout(Shout message) throws JsonGenerationException, JsonMappingException, IOException{
+	public void register(String phoneID, String userName){
+		
+	}
+	
+	public void postShout(Shout message, String location, String phoneId) throws JsonGenerationException, JsonMappingException, IOException{
 		path = "/api/shout/create";
 		ObjectMapper mapper = new ObjectMapper();
 		shoutString = mapper.writeValueAsString(message);
@@ -39,14 +51,17 @@ public class ShouterAPI {
 			public void run() {
 				ResponseEntity<String> response = null;
 				try{
-					
-					String url = Shouter_URL + path;
-					//HTTP POST
-					delegate.onShouterReturn(ShouterAPI.this, response.getBody(), null);
+					//TODO: Need to tweak to suit our API better
+					String url = Shouter_URL + path + "?location=" + location;
+					//Creates Entity with shout as body
+					HttpEntity<String> request = new HttpEntity<String>(shoutString);
+					//Post Entity to URL
+					response = REST.exchange(url, HttpMethod.POST, request, String.class);
+					delegate.onPostShoutReturn(ShouterAPI.this, response.getBody(), null);
 					
 				}catch(Exception e){
 					e.printStackTrace();
-					delegate.onShouterReturn(ShouterAPI.this, null, e);
+					delegate.onPostShoutReturn(ShouterAPI.this, null, e);
 				}
 				
 			}
@@ -54,11 +69,52 @@ public class ShouterAPI {
 	}
 	
 	public void getShout(final String Location){
+			path = "/api/shout/search";
+			
+			executor.submit(new Runnable() {
+				@Override
+				public void run() {
+					ResponseEntity<String> response = null;
+					try {
+						HttpEntity<String> request = new HttpEntity<String>();
+						
+						String url = Shouter_URL + path + "?latitute=" + latitude;
+						response = REST.exchange(url, HttpMethod.GET, request, String.class);
+						delegate.onGetShoutReturn(ShouterAPI.this, response.getBody(), null);
+					} catch(Exception e) {
+						e.printStackTrace();
+						delegate.onGetShoutReturn(ShouterAPI.this, null, e);
+					}
+				}
+			});
+	}
 		
+	
+	
+	public void postComment(String parentId, Shout message){
+
 	}
 	
-	public void getReply(final String ParentId){
+	public void getComment(final String parentId){
+		path = "/api/shout/comment/search";
 		
+		executor.submit(new Runnable() {
+			@Override
+			public void run() {
+				ResponseEntity<String> response = null;
+				try {
+					HttpEntity<String]> request = new HttpEntity<String>();
+					
+					String url = Shouter_URL + path + "?parentId=" + parentId;
+					response = REST.exchange(url, HttpMethod.GET, request, String.class);
+					delegate.onGetCommentReturn(ShouterAPI.this, response.getBody(), null);
+				} catch(Exception e) {
+					e.printStackTrace();
+					delegate.onGetCommentReturn(ShouterAPI.this, null, e);
+				}
+			}
+		});			
 	}
 	
 }
+
--- Shouter/src/com/example/shouter/util/ShouterAPIDelegate.java
+++ Shouter/src/com/example/shouter/util/ShouterAPIDelegate.java
@@ -1,5 +1,10 @@
+
 package com.example.shouter.util;
 
 public interface ShouterAPIDelegate {
-	public void onShouterReturn(ShouterAPI api, String result, Exception e);
+	public void onGetShoutReturn(ShouterAPI api, String result, Exception e);
+	public void onPostShoutReturn(ShouterAPI api, String result, Exception e);
+	public void onGetCommentReturn(ShouterAPI api, String result, Exception e);
+	public void onPostCommentReturn(ShouterAPI api, String result, Exception e);
 }
+
